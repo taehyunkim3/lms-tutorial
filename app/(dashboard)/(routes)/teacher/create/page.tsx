71ab687f85f4c5ba7d9b8fe60517ef5b1f69c2fa
TaehyunKim3 <agart319@gmail.com>
2024-01-31 13:56:37 +0900

    Feat: useForm 설치, 제출버튼 제작중

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,7 +1,95 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  title: z.string().min(3, { message: "title is REQUIRED" }).max(255),
+});
+
 const CreatePage = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/courses", values);
+      console.log(response);
+      router.push(`/dashboard/courses/${response.data.id}`);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
-    <div className="p-6">
-      <h1>Create Page</h1>
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">코스 이름을 넣으세요</h1>
+        <p className="text-sm text-slate-600 ">
+          코스 이름을 어떻게 정할지 고민하고 계신가요? <br />
+          코스 이름은 나중에 변경할 수 있습니다.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  <FormLabel>코스 이름</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="코스 이름"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    코스에서 사용할 이름을 입력하세요. 나중에 변경할 수
+                    있습니다.
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button type="button" variant="ghost">
+                  취소
+                </Button>
+              </Link>
+
+              <Button type="submit" disabled={!isValid}>
+                계속
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 };
