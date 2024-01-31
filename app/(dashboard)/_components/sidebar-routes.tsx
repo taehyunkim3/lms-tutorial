41618a52d9b345542dd9942d36fffa0a6f3a1735
TaehyunKim3 <agart319@gmail.com>
2024-01-31 13:23:56 +0900

    Feat: Dynamic layout 마무리

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import SidebarItem from "./sidebar-item";
+import { usePathname } from "next/navigation";
 const guestRoutes = [
   {
     icon: Layout,
@@ -13,8 +14,25 @@ const guestRoutes = [
     href: "/search",
   },
 ];
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Coureses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
 const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.includes("/teacher");
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
   return (
     <div className="flex flex-col w-full">
