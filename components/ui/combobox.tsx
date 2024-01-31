1eccd31091184c9655a3132619a4f57e341f9c26
TaehyunKim3 <agart319@gmail.com>
2024-01-31 18:40:35 +0900

    Feat:카테고리 선택 기능 추가

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -24,7 +24,7 @@ interface ComboboxProps {
   onChange: (value: string) => void;
 }
 
-export function ComboboxDemo({ options, value, onChange }: ComboboxProps) {
+export function Combobox({ options, value, onChange }: ComboboxProps) {
   const [open, setOpen] = React.useState(false);
 
   return (
@@ -34,7 +34,7 @@ export function ComboboxDemo({ options, value, onChange }: ComboboxProps) {
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className=" justify-between w-full"
         >
           {value
             ? options.find((option) => option.value === value)?.label
@@ -42,7 +42,7 @@ export function ComboboxDemo({ options, value, onChange }: ComboboxProps) {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full p-0">
         <Command>
           <CommandInput placeholder="Search framework..." />
           <CommandEmpty>No option found.</CommandEmpty>
