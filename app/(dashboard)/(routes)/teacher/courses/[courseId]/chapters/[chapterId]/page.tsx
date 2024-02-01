e6ed86ebb594e1ec4495399847396b8ebc326f42
TaehyunKim3 <agart319@gmail.com>
2024-02-01 13:33:35 +0900

    Feat: Chapter 수정 기능 추가(text)

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,13 +1,82 @@
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { ChapterTitleForm } from "./_components/chapter-title-form";
+
 const ChapterIdPage = async ({
   params,
 }: {
   params: { chapterId: string; courseId: string };
 }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId,
+    },
+    include: {
+      muxData: true,
+    },
+  });
+
+  if (!chapter) {
+    return redirect("/");
+  }
+
+  const requiredFields = [chapter.title, chapter.videoUrl, chapter.descroption];
+
+  const totalRequiredFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `${completedFields}/${totalRequiredFields}`;
+
+  const isComplete = requiredFields.every(Boolean);
+
   return (
-    <div>
-      chapterIdPage
-      <p>chapterId: {params.chapterId}</p>
-      <p>courseId: {params.courseId}</p>
+    <div className="p-6">
+      <aside className="flex items-center justify-between ">
+        <div className="w-full">
+          <Link
+            href={"#"}
+            className="flex items-center text-sm hover:opacity-75 mb-6"
+          >
+            <ArrowLeft className="h-4 w-4 mr-2" />
+            Back to Course Setup
+          </Link>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex flex-col gap-y-2">
+              <h1 className="text-2xl font-medium">Chapter Creation</h1>
+              <span className="text-sm text-slate-700">
+                Complete all fields {completionText}
+              </span>
+            </div>
+          </div>
+        </div>
+      </aside>
+      <article className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="space-y-4">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customise your chapter</h2>
+            </div>
+            {/* TODO:Chapter Title Form */}
+            <ChapterTitleForm
+              initialData={chapter}
+              chapterId={params.chapterId}
+              courseId={params.courseId}
+            />
+          </div>
+        </div>
+      </article>
     </div>
   );
 };
