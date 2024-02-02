f8383688a7d3fb0e07f6fc39b5c94eec91b62a0c
TaehyunKim3 <agart319@gmail.com>
2024-02-02 17:07:15 +0900

    Feat: 코스 사이드바 생성중

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -1,4 +1,10 @@
-const CourseLayout = ({
+import { getProgress } from "@/actions/get-progress";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import CourseSidebar from "./_components/course-sidebar";
+
+const CourseLayout = async ({
   children,
   params,
 }: {
@@ -7,7 +13,49 @@ const CourseLayout = ({
     courseId: string;
   };
 }) => {
-  return <div>{children}</div>;
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    redirect("/");
+  }
+
+  const progressCount = await getProgress(userId, course.id);
+
+  return (
+    <div className="h-full">
+      <div className="w-80 hidden md:flex h-full flex-col fixed  inset-y-0">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:ml-80 h-full">{children}</main>
+    </div>
+  );
 };
 
 export default CourseLayout;
