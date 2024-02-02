1ccb86dc368aa22d275eb9a1b25177829ab30dd4
TaehyunKim3 <agart319@gmail.com>
2024-02-02 17:49:28 +0900

    Feat: 동영상 좌측 챕터  버튼 생성 (course-sidebar-item)

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -1,17 +1,16 @@
-// key={chapter.id}
-// id={chapter.id}
-// label={chapter.title}
-// isCompleted={!!chapter?.userProgress?.[0]?.isCompleted}
-// courseId={course.id}
-// isLocked={!chapter.isFree && !purchase}
+"use client";
 
-type CourseSidebarItem = {
+import { cn } from "@/lib/utils";
+import { CheckCircle, Lock, PlayCircle } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+interface CourseSidebarItemProps {
   id: string;
   label: string;
   isCompleted: boolean;
   courseId: string;
   isLocked: boolean;
-};
+}
 
 const CourseSidebarItem = ({
   id,
@@ -19,8 +18,42 @@ const CourseSidebarItem = ({
   isCompleted,
   courseId,
   isLocked,
-}: CourseSidebarItem) => {
-  return <div>course sidebar item</div>;
+}: CourseSidebarItemProps) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  // prettier-ignore
+  const Icon = isLocked ?  Lock : (isCompleted ? CheckCircle : PlayCircle)
+
+  const isActive = pathname.includes(id);
+
+  const onClick = () => {
+    router.push(`/courses/${courseId}/chapters/${id}`);
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn(
+        "flex items-center pl-6 text-slate-500 font-[500] hover:bg-slate-50 text-sm",
+        isActive && " border-r-slate-600 border-r-4 bg-slate-200/20",
+        isCompleted && "text-emerald-700 hover:text-emerald-700",
+        isCompleted && isActive && "bg-emerald-50"
+      )}
+    >
+      <div className="flex items-center gap-x-2 py-4">
+        <Icon
+          className={cn(
+            " text-slate-500",
+            isActive && "text-slate-700",
+            isCompleted && "text-emerald-700"
+          )}
+        />
+        {label}
+      </div>
+    </button>
+  );
 };
 
 export default CourseSidebarItem;
