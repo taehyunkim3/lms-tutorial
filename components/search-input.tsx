a6f0f5a9ac1f138143344854227946cf97ec2f57
TaehyunKim3 <agart319@gmail.com>
2024-02-02 11:04:42 +0900

    Feat: 검색창 UI

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+
 const SearchInput = () => {
-  return <div>searchinput</div>;
+  return (
+    <div className="relative">
+      <Search className="h-4 w-4 absolute top-3 left-3 text-slate-600" />
+      <Input
+        className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200 "
+        placeholder="검색어를 입력하세요"
+      />
+    </div>
+  );
 };
 
 export default SearchInput;
