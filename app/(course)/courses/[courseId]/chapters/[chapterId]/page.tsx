5df5c83ca4e9fad1b3376581c83796b2991ae63e
TaehyunKim3 <agart319@gmail.com>
2024-02-04 11:36:25 +0900

    Feat: getChapters, chapterId페이지 제작중(actions완성, 페이지로 추출중)

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,4 +1,34 @@
-const ChapterIdPage = () => {
+import getChapter from "@/actions/get-chapter";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    courseId: params.courseId,
+    chapterId: params.chapterId,
+  });
+
   return <div className="p-40">chapter id</div>;
 };
 
