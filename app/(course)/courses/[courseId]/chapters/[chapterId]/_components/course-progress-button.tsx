e2925d650788f10a6986583883ca88693e282837
TaehyunKim3 <agart319@gmail.com>
2024-02-04 21:56:23 +0900

    Feat:mark as complete (shadcn variant 추가)

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CheckCircle, XCircle } from "lucide-react";
+
+interface CourseProgressButtonProps {
+  chapterId: string;
+  courseId: string;
+  isCompleted: boolean;
+  nextChapterId: string;
+}
+
+const CourseProgressButton = ({
+  chapterId,
+  courseId,
+  isCompleted,
+  nextChapterId,
+}: CourseProgressButtonProps) => {
+  const Icon = isCompleted ? XCircle : CheckCircle;
+  return (
+    <Button type="button" variant={isCompleted ? "outline" : "success"}>
+      {isCompleted ? "Not completed" : "Mark as Complete"}
+      <Icon className="h-4 w-4 ml-2" />
+    </Button>
+  );
+
+  //   TODO : 9:41
+};
+
+export default CourseProgressButton;
